3fa201d1d897a120dba1e578e9f581632e16037a
Andrea Canton <andreacanton@duck.com>
2023-12-08 22:06:58 +0100

    upgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://andreacanton.dev',
   trailingSlash: 'always',
-  integrations: [mdx()]
+  integrations: [mdx()],
 });
