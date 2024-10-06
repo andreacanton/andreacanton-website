2162ae2970a48d47952d6006b7858582233ea6e3
Andrea Canton <andreacanton@duck.com>
2024-10-06 18:30:21 +0200

    fix scss vite configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://andreacanton.dev',
-  trailingSlash: 'always',
+  site: "https://andreacanton.dev",
+  trailingSlash: "always",
   integrations: [mdx()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+        },
+      },
+    },
+  },
 });
